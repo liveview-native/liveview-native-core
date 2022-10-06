6346b782947505e74aea958d0ee94e42d993340c
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2022-10-06 14:55:29 -0400

    test(swift): add a simple integration test for the bindings

--- swift/LiveViewNative/Tests/LiveViewNativeTests/LiveViewNativeTests.swift
+++ swift/LiveViewNative/Tests/LiveViewNativeTests/LiveViewNativeTests.swift
@@ -1,11 +1,44 @@
 import XCTest
 @testable import LiveViewNative
 
+class MyContext {
+    var didChange = false
+}
+
 final class LiveViewNativeTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        //XCTAssertEqual(LiveViewNative().text, "Hello, World!")
+    func testIntegration() throws {
+        let context = MyContext()
+        let input = """
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+    </head>
+    <body class="new-value" class="main">
+        some content
+    </body>
+</html>
+"""
+        let doc1 = try Document.parse(input)
+        doc1.on(.changed, with: context) { doc, ctx in
+            XCTAssertEqual(ctx is MyContext, true)
+            (ctx as! MyContext).didChange = true
+        }
+
+        let updated = """
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="title" content="Hello World" />
+    </head>
+    <body class="new-value" class="main">
+        new content
+    </body>
+</html>
+"""
+        let doc2 = try Document.parse(updated)
+
+        doc1.merge(with: doc2)
+
+        XCTAssertEqual(context.didChange, true)
     }
 }
