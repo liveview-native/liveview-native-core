ca7a5f1941b5ec387c036fc978e71befbb570599
Shadowfacts <me@shadowfacts.net>
2022-10-14 15:17:50 -0400

    Re-add missing headers

---
+++ crates/core/c_src/include/LiveViewNativeCore.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "Support.h"
+
+typedef uint32_t NodeRef;
+typedef uint32_t AttributeRef;
+
+typedef struct __Document {
+  void *ptr;
+} __Document;
+
+typedef struct __Element {
+  _RustStr ns;
+  _RustStr tag;
+  _RustSlice attributes;
+} __Element;
+
+typedef struct __Attribute {
+  _RustStr ns;
+  _RustStr name;
+  _RustStr value;
+} __Attribute;
+
+enum __NodeType {
+  NodeTypeRoot = 0,
+  NodeTypeElement = 1,
+  NodeTypeLeaf = 2,
+} __attribute__((enum_extensibility(closed)));
+
+typedef enum __NodeType __NodeType;
+
+typedef union __NodeData {
+  void *root;
+  __Element element;
+  _RustStr leaf;
+} __NodeData;
+
+typedef struct __Node {
+  __NodeType ty;
+  __NodeData data;
+} __Node;
+
+extern _RustString __liveview_native_core$Document$to_string(__Document doc);
+
+extern __Document __liveview_native_core$Document$empty();
+
+extern void __liveview_native_core$Document$drop(__Document doc);
+
+extern _RustResult __liveview_native_core$Document$parse(_RustStr text,
+                                                         _RustString *error);
+
+extern bool __liveview_native_core$Document$merge(__Document doc,
+                                                  __Document other);
+
+extern NodeRef __liveview_native_core$Document$root(__Document doc);
+
+extern __Node __liveview_native_core$Document$get(__Document doc, NodeRef node);
+
+extern _RustSlice __liveview_native_core$Document$children(__Document doc,
+                                                           NodeRef node);
+
+extern _RustSlice __liveview_native_core$Document$attributes(__Document doc,
+                                                             NodeRef node);
+
+extern __Attribute
+__liveview_native_core$Document$get_attribute(__Document doc,
+                                              AttributeRef attr);
---
+++ crates/core/c_src/include/Support.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <stdbool.h>
+
+#if defined(_WIN32)
+typedef unsigned char uint8_t;
+typedef uint8_t u_int8_t;
+typedef unsigned short uint16_t;
+typedef uint16_t u_int16_t;
+typedef unsigned uint32_t;
+typedef uint32_t u_int32_t;
+typedef unsigned long long uint64_t;
+typedef uint64_t u_int64_t;
+#else
+#include <stdint.h>
+#endif
+
+typedef struct _RustResult {
+  bool is_ok;
+  void *ok_result;
+} _RustResult;
+
+typedef struct _RustSlice {
+  const void *start;
+  uintptr_t len;
+} _RustSlice;
+
+typedef struct _RustStr {
+  const void *start;
+  uintptr_t len;
+} _RustStr;
+
+extern bool __liveview_native_core$RustStr$eq(_RustStr lhs, _RustStr rhs);
+extern bool __liveview_native_core$RustStr$lt(_RustStr lhs, _RustStr rhs);
+
+typedef struct _RustString {
+  const void *start;
+  uintptr_t len;
+  uintptr_t capacity;
+} _RustString;
+
+extern void __liveview_native_core$RustString$drop(_RustString string);
