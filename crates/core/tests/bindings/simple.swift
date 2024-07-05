25e776e54cd27fd8b7f39ba25b15e7e07afdaf93
Sebastian Imlay <sebastian.imlay@gmail.com>
2024-07-05 11:42:57 -0400

    Add test coverage parts to CI

--- crates/core/tests/bindings/simple.swift
+++ crates/core/tests/bindings/simple.swift
@@ -1,4 +1,1 @@
-// The actual module name is LiveViewNativeCore but changing the module_name in
-// the uniffi.toml seems to make the phoenix-channels-clients bindings to to be
-// generated.
 import LiveViewNativeCore
