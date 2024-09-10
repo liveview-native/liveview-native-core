b3d80ee7597575c163f090c3ffc7079f8525d050
Anka <runner@Mac-1725990372260.local>
2024-09-10 17:54:32 +0000

    Update Package.swift and add generated LiveViewNativeCore.swift to tracking

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let liveview_native_core_framework: Target
 
 // To relase, toggle this to `false`
-let useLocalFramework = true
+let useLocalFramework = false
 if useLocalFramework {
     liveview_native_core_framework = .binaryTarget(
         name: "liveview_native_core",
@@ -14,7 +14,7 @@ if useLocalFramework {
     )
 } else {
     let releaseTag = "0.4.0-alpha-9"
-    let releaseChecksum = "f3972f4d40732c884c98426b28550376abaff20a3490b73367ad170f1f0bcca9"
+    let releaseChecksum = "db620d679068d7b2393fd0ce528fa701eff3dfa919fc9fd9db19acb0d936b357"
     liveview_native_core_framework = .binaryTarget(
         name: "liveview_native_core",
         url: "https://github.com/liveview-native/liveview-native-core/releases/download/\(releaseTag)/liveview_native_core.xcframework.zip",
