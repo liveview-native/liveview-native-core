2495cd84c026f1906edb9bf7abf2bc45aca3d38b
mobile-bungalow <pwmay.work@gmail.com>
2024-11-01 14:54:02 -0700

    checkpoint, no e2e tests, I think this is a bad idea

--- crates/wasm/npm_scripts/jest_mock.js
+++
@@ -1,5 +0,0 @@
-/// Substitute live view webs classes for our own during jest tests.
-jest.mock("phoenix_live_view/Rendered", () => {
-  const wasmProxy = require("liveview_native_core_wasm");
-  return wasmProxy.Rendered;
-});
--- crates/wasm/scripts/test.sh
+++ crates/wasm/scripts/test.sh
@@ -5,7 +5,7 @@ initial_dir=$(pwd)
 cleanup() {
   cd "$initial_dir"
   echo "Cleaning up..."
-  rm -rf temp_test
+  #rm -rf temp_test
 }
 
 trap cleanup ERR
@@ -14,17 +14,20 @@ rm -rf temp_test
 mkdir -p temp_test
 cd temp_test
 
-git clone https://github.com/phoenixframework/phoenix_live_view
+# git clone https://github.com/phoenixframework/phoenix_live_view
 cd phoenix_live_view/assets && npm install ../../../liveview-native-core-wasm-nodejs
 
 # shim our classes into the jest tests
 cp ../../../npm_scripts/jest_mock.js .
-npm test -- --setupFilesAfterEnv='./jest_mock.js'
+#npm test -- --setupFilesAfterEnv='./jest_mock.js'
 
 # run playwright tests
 # TODO: Shim our wasm into the playwright build
 cd ..
-npm run setup
+cp ../../npm_scripts/esbuild_shim.js .
+# below is the only relevant target from `npm run setup`
+mix deps.get
+mix esbuild module --loader: .js=./esbuild_shim.js
 npm run e2e:test
 
 cleanup
