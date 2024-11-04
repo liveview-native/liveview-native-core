1364303f6a445b83b4bdb2f8ab9cfbbc0089dace
mobile-bungalow <pwmay.work@gmail.com>
2024-11-04 10:07:34 -0800

    convenience changes for iteration

--- crates/wasm/scripts/jest_tests.sh
+++ crates/wasm/scripts/jest_tests.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-set -euo pipefail
+set -e
 
 
 initial_dir=$(pwd)
 cleanup() {
   cd "$initial_dir"
   echo "Cleaning up..."
-  #rm -rf phoenix_live_view
+  rm -rf phoenix_live_view
 }
 
 checkout_latest_tag() {
@@ -25,5 +25,7 @@ npm install ../../liveview-native-core-wasm-nodejs
 cp ../../npm_shims/jest_mock.js .
 
 # shim our classes into the jest tests
-npm test -- --setupFilesAfterEnv='./jest_mock.js'  -t "merges the latter"
+# if you need to filter tests for iteration you can add the -t argument.
+# npm test -- --setupFilesAfterEnv='./jest_mock.js' -t "merges the latter"
+npm test -- --setupFilesAfterEnv='./jest_mock.js'
 cleanup
