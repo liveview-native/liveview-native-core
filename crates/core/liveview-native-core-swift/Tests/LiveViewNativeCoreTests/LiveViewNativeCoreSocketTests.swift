1edb42201a62cf8d1d6fa13467fd4e4e52c94593
mobile-bungalow <pwmay.work@gmail.com>
2025-02-28 11:48:09 -0800

    fix swift tests

--- crates/core/liveview-native-core-swift/Tests/LiveViewNativeCoreTests/LiveViewNativeCoreSocketTests.swift
+++ crates/core/liveview-native-core-swift/Tests/LiveViewNativeCoreTests/LiveViewNativeCoreSocketTests.swift
@@ -11,32 +11,27 @@ let timeout = TimeInterval(30.0)
 let connect_url = "http://127.0.0.1:4001/hello"
 final class LiveViewNativeCoreSocketTests: XCTestCase {
     func testConnect() async throws {
-        let live_socket = try await LiveSocket(connect_url, "swiftui", .none)
-        let _ = try await live_socket.joinLiveviewChannel(.none, .none)
+        let builder = LiveViewClientBuilder()
+        let client = try await builder.connect(connect_url, ClientConnectOpts())
     }
 
     func testConnectWithOpts() async throws {
         let headers = [String: String]()
-        let options = ConnectOpts(headers: headers)
-        let live_socket = try await LiveSocket(connect_url, "swiftui", options)
-        let _ = try await live_socket.joinLiveviewChannel(.none, .none)
+        let options = ClientConnectOpts(headers: headers)
+        let builder = LiveViewClientBuilder()
+        let client = try await builder.connect(connect_url, options)
     }
 
     func testStatus() async throws {
-        let live_socket = try await LiveSocket(connect_url, "swiftui", .none)
-        let _ = try await live_socket.joinLiveviewChannel(.none, .none)
-        let socket = live_socket.socket()
+        let builder = LiveViewClientBuilder()
+        let client = try await builder.connect(connect_url, ClientConnectOpts())
 
-        var status = socket.status()
+        var status = try client.status()
         XCTAssertEqual(status, .connected)
 
-        try await socket.disconnect()
-        status = socket.status()
+        try await client.disconnect()
+        status = try client.status()
         XCTAssertEqual(status, .disconnected)
-
-        try await socket.shutdown()
-        status = socket.status()
-        XCTAssertEqual(status, .shutDown)
     }
 
     func testBasicConnection() async throws {
@@ -111,64 +106,14 @@ let base64TileImg =
 let upload_url = "http://127.0.0.1:4001/upload"
 final class LiveViewNativeCoreUploadTests: XCTestCase {
     func testUpload() async throws {
-        let live_socket = try await LiveSocket(upload_url, "swiftui", .none)
-        let live_channel = try await live_socket.joinLiveviewChannel(.none, .none)
+        // Using the new LiveViewClient API
+        let builder = LiveViewClientBuilder()
+        let client = try await builder.connect(upload_url, ClientConnectOpts())
 
         let image: Data! = Data(base64Encoded: base64TileImg)
 
-        let phx_id: String! = try live_channel.getPhxUploadId("avatar")
+        let phx_id: String! = try client.getPhxUploadId("avatar")
         let live_file = LiveFile(image, "image/png", "avatar", "foobar.png", phx_id)
-        try await live_channel.uploadFile(live_file)
+        try await client.uploadFiles([live_file])
     }
-}
-
-// Test basic navigation flow with LiveSocket
-func testBasicNavFlow() async throws {
-    let url = "http://127.0.0.1:4001/nav/first_page"
-    let secondUrl = "http://127.0.0.1:4001/nav/second_page"
-
-    let liveSocket = try await LiveSocket(url, "swiftui", .none)
-    let liveChannel = try await liveSocket.joinLiveviewChannel(.none, .none)
-
-    let doc = liveChannel.document()
-
-    let expectedFirstDoc = """
-        <Group id="flash-group" />
-        <VStack>
-            <Text>
-                first_page
-            </Text>
-            <NavigationLink id="Next" destination="/nav/next">
-                <Text>
-                    NEXT
-                </Text>
-            </NavigationLink>
-        </VStack>
-        """
-
-    let exp = try Document.parse(expectedFirstDoc)
-
-    XCTAssertEqual(doc.render(), exp.render())
-
-    let secondChannel = try await liveSocket.navigate(secondUrl, .none, NavOptions())
-
-    let secondDoc = secondChannel.document()
-
-    let expectedSecondDoc = """
-        <Group id="flash-group" />
-        <VStack>
-            <Text>
-               second_page
-            </Text>
-            <NavigationLink id="Next" destination="/nav/next">
-                <Text>
-                    NEXT
-                </Text>
-            </NavigationLink>
-        </VStack>
-        """
-
-    let secondExp = try Document.parse(expectedSecondDoc)
-
-    XCTAssertEqual(secondDoc.render(), secondExp.render())
 }
