fca1fd4601596b8f4703fc44c5b31a29fa034bf3
mobile-bungalow <pwmay.work@gmail.com>
2025-03-25 02:22:34 -0700

    jetpack weirdness

--- tests/support/test_server/lib/test_server_web/live/upload_live.ex
+++ tests/support/test_server/lib/test_server_web/live/upload_live.ex
@@ -88,8 +88,8 @@ defmodule TestServerWeb.SimpleLiveUpload.Jetpack do
     ~LVN"""
     <Box size="fill" background="system-blue">
       <Text align="Center">Upload from Jetpack</Text>
-      <LiveViewNative.Jetpack.Component.live_file_input upload={@uploads.avatar} />
-      <LiveViewNative.Jetpack.Component.live_file_input upload={@uploads.sample_text} />
+      <LiveViewNative.SwiftUI.Component.live_file_input upload={@uploads.avatar} />
+      <LiveViewNative.SwiftUI.Component.live_file_input upload={@uploads.sample_text} />
     </Box>
     """
   end
